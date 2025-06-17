10b06b895ea6dc174996c4b8f324dd802565352d
sangyunpark99 <psangyun5108@gmail.com>
2025-06-17 17:37:47 +0900

    profile

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { siteUrl } from "../../../blog-config"
+import { profile } from "../../../static/profile.png"
 
 const SEO = ({ title, description, url }) => {
   return (
@@ -8,7 +9,7 @@ const SEO = ({ title, description, url }) => {
       <title>{title}</title>
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta property="og:image" content={profile} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
